d6641b995cd90063107f96c406154bd171eeda61
Alex Gladkov <alexgladkov@icloud.com>
2026-04-26 11:39:25 -0300

    harnest: update sha256 — fixed picker

--- harnest.rb
+++ harnest.rb
@@ -7,16 +7,16 @@ class Harnest < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/AlexGladkov/harnest/releases/download/v0.3.0/harnest-v0.3.0-darwin-arm64.tar.gz"
-      sha256 "9716790e9525e881c0a3d29e99264ad62aae77ce32c738a4505bded62eaefa16"
+      sha256 "55acdba04adb758c3d2909f163279b6fb9c8c82d552bb4ca2763fbb1df8490d3"
     else
       url "https://github.com/AlexGladkov/harnest/releases/download/v0.3.0/harnest-v0.3.0-darwin-amd64.tar.gz"
-      sha256 "50f6f51e1be8e63c721db4277ab5f267d41fc5dbe71c3614518cec2d996d41dc"
+      sha256 "ecb5df8d4e70da07cb4ad6723703c5a4716dac348af40d770aab3649a8f07bb8"
     end
   end
 
   on_linux do
     url "https://github.com/AlexGladkov/harnest/releases/download/v0.3.0/harnest-v0.3.0-linux-amd64.tar.gz"
-    sha256 "f342df5de03da177a488213cf6e27adbb5ce18e29cc5861213a40fd063862713"
+    sha256 "1ace1184b3dadc8d35befdebe037cad1cf33a90e2f05b06ae15604bdde52ed78"
   end
 
   def install

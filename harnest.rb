e93bf0882d49d34ac3197f05c0237ab0ad650841
Alex Gladkov <alexgladkov@icloud.com>
2026-04-26 10:38:05 -0300

    harnest: update sha256 (stage pool wizard)

--- harnest.rb
+++ harnest.rb
@@ -7,16 +7,16 @@ class Harnest < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/AlexGladkov/harnest/releases/download/v0.3.0/harnest-v0.3.0-darwin-arm64.tar.gz"
-      sha256 "939d00b7cc82293a2d09928b7cddcd935996a82347b41d89fb7356a02ed5b252"
+      sha256 "f1c2c3e43d4fa19f4391c3397f444fd98e909e7f0bd344a27f87f0b3f29049a5"
     else
       url "https://github.com/AlexGladkov/harnest/releases/download/v0.3.0/harnest-v0.3.0-darwin-amd64.tar.gz"
-      sha256 "94fe9db5ead6981732994cceab9ce2d200852812c2f2c1f2f1900e5623fbfcc2"
+      sha256 "9436c4d1e2eeed9e9d7c0586fc5f03c3ae87e396f5a25155ecedc8d83d1e52cf"
     end
   end
 
   on_linux do
     url "https://github.com/AlexGladkov/harnest/releases/download/v0.3.0/harnest-v0.3.0-linux-amd64.tar.gz"
-    sha256 "5f408d4e9138029e41ff43adec682e4b8ee075900f749c0f4d626f8597e08c88"
+    sha256 "168042859857953de222b17fcd7e3cafdfae5c3014b3004f0fe0440048075171"
   end
 
   def install

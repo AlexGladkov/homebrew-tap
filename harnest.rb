b4514e0b01eb80ee9770ca4ff49309471401b65d
Alex <mobiledevelopercourse@gmail.com>
2026-04-27 10:21:36 +0300

    Update harnest to v0.5.0

--- harnest.rb
+++ harnest.rb
@@ -1,22 +1,22 @@
 class Harnest < Formula
   desc "AI coding assistant configurator - detects stack, generates agent configs"
   homepage "https://github.com/AlexGladkov/harnest"
-  version "0.4.0"
+  version "0.5.0"
   license "CC-BY-NC-4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/AlexGladkov/harnest/releases/download/v0.4.0/harnest-v0.4.0-darwin-arm64.tar.gz"
-      sha256 "4fa22af7f01709cbd4c68f9852c33cb52d0f8483c35beb4d15980a428dd3b260"
+      url "https://github.com/AlexGladkov/harnest/releases/download/v0.5.0/harnest-v0.5.0-darwin-arm64.tar.gz"
+      sha256 "b9f8454d7205b42179115fb7559d81307ecf563c8f3aef06638efa2e35c84fc8"
     else
-      url "https://github.com/AlexGladkov/harnest/releases/download/v0.4.0/harnest-v0.4.0-darwin-amd64.tar.gz"
-      sha256 "e894b739d202b1046cf3ed65dffec614040bd2d05f53a901d32952d526e1065f"
+      url "https://github.com/AlexGladkov/harnest/releases/download/v0.5.0/harnest-v0.5.0-darwin-amd64.tar.gz"
+      sha256 "ca9d01f15260256d4d7062d0bff46e765ddfea3614a595e784b74f08edc5bc29"
     end
   end
 
   on_linux do
-    url "https://github.com/AlexGladkov/harnest/releases/download/v0.4.0/harnest-v0.4.0-linux-amd64.tar.gz"
-    sha256 "bdf4a400899714f546a896e5b71f4c7f20a99d3c79d3b05670e72af18969c794"
+    url "https://github.com/AlexGladkov/harnest/releases/download/v0.5.0/harnest-v0.5.0-linux-amd64.tar.gz"
+    sha256 "cafc8718bfe13978bf15e61c4de3fe6b5a6271ffb8d07f641e05ea174d31cdad"
   end
 
   def install

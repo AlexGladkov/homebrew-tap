eca47a715c5302ece48f364853e7c13c7aec20cb
Alex Gladkov <alexgladkov@icloud.com>
2026-06-16 00:07:01 +0300

    tokenplace 0.29.0

--- tokenplace.rb
+++ tokenplace.rb
@@ -4,27 +4,27 @@
 class Tokenplace < Formula
   desc "CLI-first AI infrastructure marketplace for the Russian engineering market"
   homepage "https://github.com/AlexGladkov/tokenplace"
-  version "0.28.0"
+  version "0.29.0"
 
   depends_on "ollama"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.28.0/tokenplace-0.28.0-aarch64-apple-darwin.tar.gz"
-      sha256 "8b512e43f7ccde99ade13b9cf4c06a966f293dbe44bbb2ce341bdd434678e98a"
+      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.29.0/tokenplace-0.29.0-aarch64-apple-darwin.tar.gz"
+      sha256 "d03f7cd4c226bc953c9363da1abd9f14e7797f7efba8a6df1930111256d6b233"
     else
-      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.28.0/tokenplace-0.28.0-x86_64-apple-darwin.tar.gz"
-      sha256 "8b512e43f7ccde99ade13b9cf4c06a966f293dbe44bbb2ce341bdd434678e98a"
+      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.29.0/tokenplace-0.29.0-x86_64-apple-darwin.tar.gz"
+      sha256 "d03f7cd4c226bc953c9363da1abd9f14e7797f7efba8a6df1930111256d6b233"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.28.0/tokenplace-0.28.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8b512e43f7ccde99ade13b9cf4c06a966f293dbe44bbb2ce341bdd434678e98a"
+      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.29.0/tokenplace-0.29.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d03f7cd4c226bc953c9363da1abd9f14e7797f7efba8a6df1930111256d6b233"
     else
-      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.28.0/tokenplace-0.28.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8b512e43f7ccde99ade13b9cf4c06a966f293dbe44bbb2ce341bdd434678e98a"
+      url "https://github.com/AlexGladkov/homebrew-tap/releases/download/tokenplace-v0.29.0/tokenplace-0.29.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d03f7cd4c226bc953c9363da1abd9f14e7797f7efba8a6df1930111256d6b233"
     end
   end
 
